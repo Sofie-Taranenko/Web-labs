a54740a3c66ab208d2838c6b2e2e495113da21c4
София Тараненко <taransofa44@gmail.com>
2022-12-29 00:36:10 +0700

    Переключения состояний

--- lab9.js
+++ lab9.js
@@ -11,3 +11,25 @@ function clickFIO(button) {
     else 
         button.innerHTML = 'София Тараненко'; 
 } 
+ 
+function knopka(button) { 
+    if(button.innerHTML == 'Нажми меня') { 
+        button.innerHTML = 'Ещё раз нажми'; 
+        button.style.color = 'purple'; 
+    } 
+         
+    else if(button.innerHTML == 'Ещё раз нажми') { 
+        button.innerHTML = 'Ещё';   
+        button.style.color = 'red'; 
+    } 
+          
+    else if(button.innerHTML == 'Ещё') { 
+        button.innerHTML = 'Начать сначала';    
+        button.style.color = 'orange'; 
+    } 
+         
+    else { 
+        button.innerHTML = 'Нажми меня'  
+        button.style.color = 'blue';  
+    } 
+}
