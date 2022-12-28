e07a794c5ca90f051a9605783ed67dba686bf174
София Тараненко <taransofa44@gmail.com>
2022-12-29 00:47:53 +0700

    Фокус

--- lab9.js
+++ lab9.js
@@ -43,3 +43,21 @@ window.addEventListener('DOMContentLoaded', function() {
     let coords = document.querySelector('#coords'); 
     coords.addEventListener('mousemove', showCoords); 
 });
+
+function showFocus(event) { 
+    event.target.style.color = 'purple'; 
+    event.target.style.fontSize = "25px"; 
+} 
+function showBlur(event) { 
+    event.target.style.color = 'black'; 
+    event.target.style.fontSize = "14px"; 
+} 
+window.addEventListener('DOMContentLoaded', function() { 
+    let texts = document.querySelectorAll('input[type="text"]'); 
+    texts[0].addEventListener('focus', showFocus); 
+    texts[1].addEventListener('focus', showFocus); 
+    texts[2].addEventListener('focus', showFocus); 
+    texts[0].addEventListener('blur', showBlur); 
+    texts[1].addEventListener('blur', showBlur); 
+    texts[2].addEventListener('blur', showBlur); 
+}); 
