5a6f4c2291a3b472879cebf67d01971534a72465
София Тараненко <taransofa44@gmail.com>
2022-12-29 00:30:17 +0700

    Кнопка Имя-Фамилия

--- lab9.js
+++ lab9.js
@@ -4,3 +4,10 @@ function clickMe(button) {
     else 
         button.innerHTML = 'Щелчок'; 
 } 
+
+function clickFIO(button) { 
+    if(button.innerHTML == 'София Тараненко') 
+        button.innerHTML = 'Тараненко София'; 
+    else 
+        button.innerHTML = 'София Тараненко'; 
+} 
